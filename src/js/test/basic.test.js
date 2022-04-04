64ab5e62ff321151dfcbbd609bac0f10934161da
KostiaevSV <skprod.info@gmail.com>
2022-04-04 10:35:14 +0400

    change test.js

--- src/js/test/basic.test.js
+++ src/js/test/basic.test.js
@@ -1,18 +1,18 @@
-import { TestWatcher } from 'jest';
 import getStatusOfHealth from '../app';
 
-test('status healthy', () => {
-    const result = getStatusOfHealth({ name: 'Маг', health: 80 });
-    expect(result).toBe('healthy');
+test('should status healthy', () => {
+  const result = getStatusOfHealth({ name: 'f', health: 80 });
+  expect(result).toBe('healthy');
 });
 
-test('status wounded', () => {
-    const result = getStatusOfHealth({ name: 'Маг', health: 45 });
-    expect(result).toBe('wounded');
-  });
-  
-  
-  test('status critical', () => {
-    const result = getStatusOfHealth({ name: 'Маг', health: 10 });
-    expect(result).toBe('critical');
-  });
+
+test('should status wounded', () => {
+  const result = getStatusOfHealth({ name: 'f', health: 45 });
+  expect(result).toBe('wounded');
+});
+
+
+test('should status critical', () => {
+  const result = getStatusOfHealth({ name: 'f', health: 10 });
+  expect(result).toBe('critical');
+});
